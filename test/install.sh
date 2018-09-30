6bab0571e27b5009127d4afb4a8786e9c12ada47
finvkus <basov.yo1984@gmail.com>
2018-09-30 21:52:14 +0300

    initial commit

--- test/install.sh
+++ test/install.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 set -e -x
-echo "===== STEP1 ====="
+
+echo "===== STEP0 ====="
 sudo apt -y install curl
 echo "[OK]"
+echo "===== STEP1 ====="
+wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+echo "[OK]"
 echo "===== STEP2 ====="
-sudo add-apt-repository "deb https://cliassets.heroku.com/branches/stable/apt ./"
+#sudo add-apt-repository "deb https://cliassets.heroku.com/branches/stable/apt ./"
 echo "[OK]"
 echo "===== STEP3 ====="
-curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+#curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
 echo "[OK]"
 echo "===== STEP4 ====="
-sudo apt-get update
+#sudo apt-get update
 echo "[OK]"
 echo "===== STEP5 ====="
-sudo apt-get install heroku
+#sudo apt-get install heroku
 echo "[OK]"
